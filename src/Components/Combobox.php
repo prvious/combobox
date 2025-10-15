c4fb96b593b1238502a856376691c79ba5f96657
Clovis Muneza <mugaro234@gmail.com>
2025-10-15 03:51:41 -0400

    🏃🏾‍♂️💨 wip

--- src/Components/Combobox.php
+++ src/Components/Combobox.php
@@ -4,4 +4,10 @@ namespace Prvious\Filament\Combobox\Components;
 
 use Filament\Forms\Components\Select;
 
-class Combobox extends Select {}
+class Combobox extends Select
+{
+    /**
+     * @var view-string
+     */
+    protected string $view = 'filament-forms::components.select';
+}
